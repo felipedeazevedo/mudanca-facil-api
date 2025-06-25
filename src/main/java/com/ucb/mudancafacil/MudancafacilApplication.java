dca39363cd52a500cd5bc1840caf6a1c42d9db5a
Felipe Azevedo <Felipe.Azevedo@partner.bmwgroup.com>
2025-06-25 16:34:26 -0300

    feat: sobe scaffolding

---
+++ src/main/java/com/ucb/mudancafacil/MudancafacilApplication.java
@@ -0,0 +1,13 @@
+package com.ucb.mudancafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudancafacilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MudancafacilApplication.class, args);
+    }
+
+}
